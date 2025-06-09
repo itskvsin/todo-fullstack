607604b471068b26f6e219ee55c79efe91a685a4
Kevin <kevinsolankistudy@gmail.com>
2025-06-09 18:58:57 +0530

    Configured Tailwind

--- frontend/src/components/CreateTodo.jsx
+++ frontend/src/components/CreateTodo.jsx
@@ -1,9 +1,10 @@
 
 const CreateTodo = () => {
     return (
-        <div>
-            <input type="text" name="title" id="title"  placeholder="Title: "/>
-            <input type="text" name="description" id="description" placeholder="Description: "/>
+        <div className="flex flex-col w-2/4 m-auto gap-6 items-center">
+            <input className="border-2 w-full border-black outline-none p-3 rounded-lg bg-gray-200" type="text" name="title" id="title"  placeholder="Title: "/>
+            <input className="border-2 w-full border-black outline-none p-3 rounded-lg bg-gray-200" type="text" name="description" id="description" placeholder="Description: "/>
+            <button className="bg-black text-white w-1/4 p-2 rounded cursor-pointer">Submit</button>
         </div>
     )
 }
