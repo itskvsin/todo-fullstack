88418ee123c633ca33bc913eb8f10c407aa013ec
Kevin <kevinsolankistudy@gmail.com>
2025-06-09 18:43:02 +0530

    made some basic changes in the app

---
+++ frontend/src/components/CreateTodo.jsx
@@ -0,0 +1,11 @@
+
+const CreateTodo = () => {
+    return (
+        <div>
+            <input type="text" name="title" id="title"  placeholder="Title: "/>
+            <input type="text" name="description" id="description" placeholder="Description: "/>
+        </div>
+    )
+}
+
+export default CreateTodo;
